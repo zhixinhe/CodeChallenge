0bab87a62ff363211e310463ccb9b2667778ffad
Zhixin He <zhixin.he@hotmail.com>
2022-06-20 13:14:04 -0700

    optimized to be O(1)

--- 0620_SherlockAndSquare/Program.cs
+++ 0620_SherlockAndSquare/Program.cs
@@ -1,14 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Solution;
 class Program
 {
     public static int squares(int a, int b) {
-        int count = 0;
-        for (int i = 0; i*i <= b; i++) {
-            if (i * i <= b && i * i >= a) {
-                count++;
-            }
-        }
-        return count;
+        //int count = 0;
+        // for (int i = 0; i*i <= b; i++) {
+            // if (i * i <= b && i * i >= a) {
+                // count++;
+            // }
+        // }
+        int sq = (int)Math.Ceiling(Math.Sqrt(a));
+        int bq = (int)Math.Floor(Math.Sqrt(b));
+        return bq - sq + 1;
     }
     public static void Main(string[] args)
     {
