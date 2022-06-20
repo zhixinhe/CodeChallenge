de08a1414bac6c1ac612727a1b69b35bbb2ba35a
Zhixin He <zhixin.he@hotmail.com>
2022-06-20 12:53:39 -0700

    06/20/2022 completed PO's Weekly Coding Challenge

---
+++ 0620_SherlockAndSquare/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static int squares(int a, int b) {
+        int count = 0;
+        for (int i = 0; i*i <= b; i++) {
+            if (i * i <= b && i * i >= a) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int result = squares(a, b);
+        Console.WriteLine(result);
+    }
+}
